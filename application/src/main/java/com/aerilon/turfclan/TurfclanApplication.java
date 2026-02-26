13d4ee93cc3d927671c4486e0f9d423fc36a882b
Rajat Singh <rajat.singh@aerilontech.com>
2026-02-26 14:49:26 +0530

    initial module and project setup

---
+++ application/src/main/java/com/aerilon/turfclan/TurfclanApplication.java
@@ -0,0 +1,13 @@
+package com.aerilon.turfclan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurfclanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurfclanApplication.class, args);
+	}
+
+}
---
+++ scripts/run-app.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set SPRING_PROFILE_ACTIVE=local
+set APP_COPYRIGHT=2026 Aerilon Tech LLP Solutions, Inc. All rights reserved.
+set ALLOWED_ORIGIN=*
+set LOG_LEVEL=INFO
+set SPRINGFRAMEWORK_WEB_LOG_LEVEL=ERROR
+set AWS_RDS_URL=jdbc:postgresql://localhost:5432/turfclan
+set RDS_SCHEMA=turfclan_schema
+set RDS_USERNAME=aerilon_user_2
+set RDS_PASSWORD=aerilon@123
+
+call .\gradlew.bat clean
+call .\gradlew.bat bootRun --debug-jvm
